b350e034d168ac5ab7996a209204c3beae54fc5f
Azelat <Azel4at@gmail.com>
2018-11-13 22:13:27 +0100

    Main

--- src/capitalism/Capitalism.java
+++ src/capitalism/Capitalism.java
@@ -9,12 +9,21 @@ import capitalism.IHM.Cases.ListeCase;
 import capitalism.IHM.Interface.Bandeau;
 import capitalism.IHM.Interface.BoutonMenuList;
 import capitalism.Metier.Jeu;
+import capitalism.Metier.Parties.Carte.Cases.Case;
 import capitalism.Metier.Parties.Carte.Map;
 import capitalism.Metier.Parties.Carte.Parseur.ParseurMap;
+import capitalism.Metier.Parties.Partie;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -24,13 +33,22 @@ import javafx.stage.Stage;
  */
 public class Capitalism extends Application {
     
+    private double X;
+    private double Y;
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         
-        StackPane root = new StackPane();   
+        Jeu j = new Jeu();
+        j.newGame();
+        Map m = j.getPartie().getMap();
+        m.chargerFichier("Carte.txt");
+        /*System.out.println(j.getPartie().getMap().getListeCases());*/
+        ArrayList<Case> listeCase = j.getPartie().getMap().getListeCases();
+        Pane root = new Pane(); 
         Scene scene = new Scene(root, 1200, 800);
         BoutonMenuList bl = new BoutonMenuList();
-        ListeCase l = new ListeCase();
+        
+        ListeCase l = new ListeCase(j, m, listeCase);
         root.getChildren().add(l);
         Bandeau b = new Bandeau();
         root.getChildren().add(bl); 
@@ -39,7 +57,6 @@ public class Capitalism extends Application {
         primaryStage.setTitle("Capitalisme");
         primaryStage.setScene(scene);
         primaryStage.show();
-        
     }
 
     /**
