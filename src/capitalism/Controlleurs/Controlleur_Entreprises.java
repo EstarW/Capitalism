b40d7da8c1d46a418ccf858c920404e02a6f098c
Boukii <k.hedhie@gmail.com>
2018-12-10 18:51:07 +0100

    Controlleur entreprise

--- src/capitalism/Controlleurs/Controlleur_Entreprises.java
+++ src/capitalism/Controlleurs/Controlleur_Entreprises.java
@@ -25,12 +25,12 @@ import javafx.scene.layout.StackPane;
 public class Controlleur_Entreprises {
     
     private Entreprise model;
-    private StackPane vue;
+    private CreationPartieController vue;
             
     
     //---------- CONSTRUCTEURS -----------------------------------------------------
 
-    public Controlleur_Entreprises(Entreprise _model, StackPane _vue){
+    public Controlleur_Entreprises(Entreprise _model, CreationPartieController _vue){
         //Initialisation
         this.model=_model;
         this.vue = _vue;
