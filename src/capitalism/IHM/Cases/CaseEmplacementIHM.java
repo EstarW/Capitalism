42da58a379f4c3418c55d57731c5a8ad66ac1642
EstarW <39602218+EstarW@users.noreply.github.com>
2019-01-03 15:41:54 +0100

    Update CaseEmplacementIHM.java

--- src/capitalism/IHM/Cases/CaseEmplacementIHM.java
+++ src/capitalism/IHM/Cases/CaseEmplacementIHM.java
@@ -5,11 +5,9 @@
  */
 package capitalism.IHM.Cases;
 
-import capitalism.IHM.WindowsCode.Game;
 import capitalism.IHM.WindowsCode.MenuContextuel;
 import capitalism.IHM.WindowsCode.MenuCreationUsine;
-import capitalism.Metier.Parties.Carte.Map;
-import capitalism.Metier.Parties.Entreprises.Entreprise;
+import capitalism.Metier.Parties.Carte.Cases.CaseEmplacement;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -27,12 +25,6 @@ import javafx.scene.shape.Rectangle;
  */
 public class CaseEmplacementIHM extends CaseIHM {
     
-    private double X;
-    private double Y;
-    private double ligne;
-    private double colonne;
-    private boolean state;
-    private boolean dialog;
     private MenuContextuel m;
     private MenuCreationUsine m2;
     private Rectangle rec;
@@ -40,38 +32,28 @@ public class CaseEmplacementIHM extends CaseIHM {
     private ImageView imgv;
     private Image img2;
     private ImageView imgv2;
-    private Entreprise j;
-    private Game g;
-    private Map map;
     
-    public CaseEmplacementIHM(double X, double Y, Entreprise j, Map map, Game g) throws IOException {
-        super(X, Y);
-        this.j = j;
-        this.g = g;
-        this.map = map;
+    public CaseEmplacementIHM(CaseEmplacement c) throws IOException {
+        super(c);
         rec = new Rectangle(50,50,Color.GREY);
         rec.setStroke(Color.rgb(0,0,0, 0.5));
         rec.setStrokeWidth(1);
-        rec.setX(X*50);
-        rec.setY(Y*50);
-        state = false;
-        dialog = false;
-        ligne = X;
-        colonne = Y;
+        rec.setX(this.getX()*50);
+        rec.setY(this.getY()*50);
         this.getChildren().add(rec);
         
         FileInputStream inputstream = new FileInputStream("..\\Capitalism\\src\\capitalism\\Resources\\Sprites\\Neige.png"); 
         img = new Image(inputstream); 
         imgv = new ImageView(img);
-        imgv.setX(X*50);
-        imgv.setY(Y*50);
+        imgv.setX(this.getX()*50);
+        imgv.setY(this.getY()*50);
         this.getChildren().add(imgv);
         
         FileInputStream inputstream2 = new FileInputStream("..\\Capitalism\\src\\capitalism\\Resources\\Sprites\\EmplacementUsine.png"); 
         img2 = new Image(inputstream2); 
         imgv2 = new ImageView(img2);
-        imgv2.setX(X*50);
-        imgv2.setY(Y*50);
+        imgv2.setX(this.getX()*50);
+        imgv2.setY(this.getY()*50);
         this.getChildren().add(imgv2);
         
            
@@ -105,48 +87,5 @@ public class CaseEmplacementIHM extends CaseIHM {
 
         });
     }
-    
-    
-    public double getX() {
-        return X;
-    }
-
-    public double getY() {
-        return Y;
-    }
-    
-    public double getLigne(){
-        return ligne;
-    }
-    
-    public double getColonne(){
-        return colonne;
-    }
-
-    public void setX(double X) {
-        this.X = X*50;
-    }
-
-    public void setY(double Y) {
-        this.Y = Y*50;
-    }
-    
-    public boolean getState(){
-        return state;
-    }
-    
-    public void setState(boolean s) throws FileNotFoundException{
-        state = s;
-        if(state == true){
-            this.getChildren().remove(imgv2);
-            FileInputStream inputstream2 = new FileInputStream("..\\Capitalism\\src\\capitalism\\Resources\\Sprites\\UsineRouge1.png"); 
-            img2 = new Image(inputstream2); 
-            imgv2 = new ImageView(img2);
-            imgv2.setX(ligne*50);
-            imgv2.setY(colonne*50);
-            this.getChildren().add(imgv2);
-        }
-    }
-    
     
 }
